81576a1d66ea128eb68e1439fa51bee279cb5df7
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-02-15 19:32:02 +0000

    got one sql query working fine

--- src/main/java/ie/nuigalway/sd3/configurations/ApplicationConfiguration.java
+++ src/main/java/ie/nuigalway/sd3/configurations/ApplicationConfiguration.java
@@ -1,18 +1,43 @@
 package ie.nuigalway.sd3.configurations;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 @ComponentScan(basePackages = "ie.nuigalway.sd3")
 public class ApplicationConfiguration {
 
+	@Autowired
+	private Environment env;
+
 	@Bean
 	public String myMessage( @Value("${my.messageValue}") String messageValue ){
 
 		return messageValue;
+	}
+
+
+	//our production mysql connection
+	@Bean(name = "dataSource")
+	@Profile("prod")
+	public DataSource dataSourceForProd() {
+
+		BasicDataSource dataSource = new BasicDataSource();
+
+		dataSource.setDriverClassName(env.getProperty("db.driver"));
+		dataSource.setUrl(env.getProperty("db.url"));
+		dataSource.setUsername(env.getProperty("db.user"));
+		dataSource.setPassword(env.getProperty("db.pass"));
+
+		return dataSource;
 	}
 }
--- src/main/java/ie/nuigalway/sd3/repositories/MysqlThreadRepository.java
+++ src/main/java/ie/nuigalway/sd3/repositories/MysqlThreadRepository.java
@@ -18,36 +18,61 @@ import java.util.List;
 @Profile("prod")
 public class MysqlThreadRepository implements ThreadRepository{
 
-
+	//our jdbc tempate
 	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public MysqlThreadRepository( DataSource dataSource ){
+
+		jdbcTemplate = new JdbcTemplate( dataSource );
+	}
+
 
 	@Override
 	public List<Thread> getThreads() {
 
+		String sqlTxt = "SELECT * FROM threads";
+		return jdbcTemplate.query(sqlTxt, new ThreadMapper());
 	}
+
 
 	@Override
 	public Thread getThread(Long id) {
 
+		String sqlTxt = "SELECT * FROM threads WHERE id=?";
+		return jdbcTemplate.queryForObject(sqlTxt, new ThreadMapper(), id);
 	}
 
 	@Override
 	public int getNumberOfThreads() {
 
+		return 0; //TODO
 	}
 
 	@Override
 	public Long createThread(String title) {
 
+		return 0L; //TODO
 	}
 
 	@Override
 	public void deleteThread(Long id) {
 
+		//TODO
 	}
 
 	@Override
 	public void updateThread(Thread thread) {
 
+		//TODO
+	}
+
+
+	private class ThreadMapper implements RowMapper<Thread> {
+
+		@Override
+		public Thread mapRow(ResultSet resultSet, int i) throws SQLException {
+			return new Thread( resultSet.getLong("id"), resultSet.getString("title") );
+		}
 	}
 }
