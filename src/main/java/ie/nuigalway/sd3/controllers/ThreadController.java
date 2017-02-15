acefbb1c8ed7afda68e22868650194cf7d59d244
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-02-15 18:43:01 +0000

    api thread controller started work

--- src/main/java/ie/nuigalway/sd3/controllers/ThreadController.java
+++ src/main/java/ie/nuigalway/sd3/controllers/ThreadController.java
@@ -3,8 +3,9 @@ package ie.nuigalway.sd3.controllers;
 import ie.nuigalway.sd3.entities.Thread;
 import ie.nuigalway.sd3.services.ThreadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
@@ -15,7 +16,7 @@ public class ThreadController {
 	@Autowired
 	private ThreadService threadService;
 
-	@RequestMapping("/api/threads");
+	@RequestMapping(method= RequestMethod.GET, value="/api/threads")
 	public List<Thread> getThreads(){
 
 		return threadService.getThreads();
