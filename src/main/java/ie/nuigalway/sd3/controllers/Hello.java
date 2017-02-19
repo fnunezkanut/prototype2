68e6cad38869645e60585a44c2937b18b4b66514
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-02-19 17:03:32 +0000

    started on main page

---
+++ src/main/java/ie/nuigalway/sd3/controllers/Hello.java
@@ -0,0 +1,14 @@
+package ie.nuigalway.sd3.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hello {
+
+	@RequestMapping("/hello")
+	public String hello(){
+
+		return "hello";
+	}
+}
