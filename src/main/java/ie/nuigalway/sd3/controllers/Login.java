c62d5b5c8aee615b8b30b8c93df382accd4e8c90
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-03-03 17:43:20 +0000

    sorted out favicon and main template

--- src/main/java/ie/nuigalway/sd3/controllers/Login.java
+++ src/main/java/ie/nuigalway/sd3/controllers/Login.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Homepage {
+public class Login {
 
-	@RequestMapping("/")
+	@RequestMapping("/login")
 	public String subaction(){
 
-		return "homepage";
+		return "login";
 	}
 }
