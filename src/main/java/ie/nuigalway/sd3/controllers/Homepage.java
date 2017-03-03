1ed0122e4de04a683723460617777b4049fc636e
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-03-03 18:07:35 +0000

    added random uuid

--- src/main/java/ie/nuigalway/sd3/controllers/Homepage.java
+++ src/main/java/ie/nuigalway/sd3/controllers/Homepage.java
@@ -1,13 +1,21 @@
 package ie.nuigalway.sd3.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Homepage {
 
+	@Value("${my.uuid}")
+	String myUuid;
+
 	@RequestMapping("/")
-	public String subaction(){
+	public String subaction( ModelMap model ){
+
+		//pass a random uuid to the view template
+		model.addAttribute("randomUuid", myUuid );
 
 		return "homepage";
 	}
