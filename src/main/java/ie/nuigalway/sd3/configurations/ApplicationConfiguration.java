e84461e7546c4fe15a23b4096cfe68f92f94a1da
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-03-03 15:38:15 +0000

    added jtwig hot reload plugin

--- src/main/java/ie/nuigalway/sd3/configurations/ApplicationConfiguration.java
+++ src/main/java/ie/nuigalway/sd3/configurations/ApplicationConfiguration.java
@@ -1,18 +1,25 @@
 package ie.nuigalway.sd3.configurations;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.jtwig.environment.EnvironmentConfigurationBuilder;
+import org.jtwig.spring.JtwigViewResolver;
+import org.jtwig.spring.boot.config.JtwigViewResolverConfigurer;
+import org.jtwig.web.servlet.JtwigRenderer;
+import org.jtwig.hot.reloading.HotReloadingExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "ie.nuigalway.sd3")
-public class ApplicationConfiguration {
+public class ApplicationConfiguration implements JtwigViewResolverConfigurer {
 
 	@Autowired
 	private Environment env;
@@ -30,5 +37,21 @@ public class ApplicationConfiguration {
 		dataSource.setPassword(env.getProperty("db.pass"));
 
 		return dataSource;
+	}
+
+	//configures jtwig
+	@Override
+	public void configure( JtwigViewResolver viewResolver ) {
+
+		viewResolver.setRenderer(
+				new JtwigRenderer(
+					EnvironmentConfigurationBuilder.configuration()
+													.extensions()
+													.add(new HotReloadingExtension())
+													.and()
+													.build()
+			)
+		);
+		viewResolver.setSuffix(".twig");
 	}
 }
